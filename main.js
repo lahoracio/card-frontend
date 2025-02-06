4e13e09e5ff9fe45ebb87361f2b244a77eb92b9f
LARA MACHADO HORÁCIO <24122307@CFP127ALUNO.LOCAL>
2025-02-06 15:34:32 -0300

    primeira parte do exercicio pronto

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 'use strict'
 
-function criarMenu(){
-    const novoItem = document.createElement('li')
-    const novaImagem = document.createElement('img')
-    const novoSpan = document.createElement('Span')
-    const lista = document.getElementById('menu')
+const categorias = [
+    {nome: 'Celulares', icon: 'celular.png', cor: 'red'},
+    {nome: 'Informatica', icon: 'notebook.png', cor: 'plum'},
+    {nome: 'Games', icon: 'gamer.png', cor: 'red'},
+    {nome: 'Móveis', icon: 'cama.png', cor: 'blue'}
+]
+
+
+function criarMenu(categoria){
+const novoItem = document.createElement('li')
+const novaImagem = document.createElement('img')
+const novoSpan = document.createElement('Span')
+const lista = document.getElementById('menu')
+
+novaImagem.src = `./img/${categoria.icon}`
+novoSpan.textContent = categoria.nome
+
+novoItem.appendChild(novaImagem)
+novoItem.appendChild(novoSpan)
+novoItem.style = `--cor-hover:${categoria.cor}`
+
+lista.appendChild(novoItem)
+
 }
+
+categorias.forEach(criarMenu)
+
+
+
