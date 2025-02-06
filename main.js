8cd58a49397dd4b2e7a7435f8b82edc93128485b
LARA MACHADO HORÁCIO <24122307@CFP127ALUNO.LOCAL>
2025-02-06 14:20:34 -0300

    css atualizado

---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function criarMenu(){
+    const novoItem = document.createElement('li')
+    const novaImagem = document.createElement('img')
+    const novoSpan = document.createElement('Span')
+    const lista = document.getElementById('menu')
+}
